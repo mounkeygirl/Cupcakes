829b468e381a7c2b53e009df1a2d1663562d1d84
mounkeygirl <mounkeygirl@yahoo.com>
2021-01-04 15:55:40 -0600

    Strawberry Attempt

--- angular-strawberry/karma.conf.js
+++ angular-strawberry/karma.conf.js
@@ -1,25 +1,18 @@
-// process.env.CHROME_BIN = require('puppeteer').executablePath()
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
+process.env.CHROME_BIN = require('puppeteer').executablePath()
 
 module.exports = function (config) {
   config.set({
     basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular', "karma-typescript"],
-    files: [
-      "src/**/*.ts" // *.tsx for React Jsx
-  ],
-  preprocessors: {
-    "**/*.ts": "karma-typescript" // *.tsx for React Jsx
-},
-    
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
+      // require('puppeteer'),
+      // require("@types/puppeteer"),
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage'),
-      require('karma-typescript'),
-      require('karma-firefox-launcher'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
@@ -35,41 +28,26 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/angular-muffin'),
+      dir: require('path').join(__dirname, './coverage/angular-strawberry'),
       subdir: '.',
       reporters: [
         { type: 'html' },
         { type: 'text-summary' }
       ]
     },
-    reporters: ['progress', 'kjhtml',"karma-typescript"],
+    reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    // browsers: ['Chrome'],
-    browsers: ['Chrome','ChromeHeadlessCI','Chrome_without_security','ChromeHeadless','FirefoxHeadless'],
-    customLaunchers: {
-      ChromeHeadlessCI: {
-        base: 'ChromeHeadless',
-        flags: ['--no-sandbox']
-  },
-  Chrome_without_security: {
-    base: 'Chrome',
-    flags: ['--disable-web-security', '--disable-site-isolation-trials']
-  },
-  'FirefoxHeadless': {
-    base: 'Firefox',
-    flags: [
-        '-headless',
-    ],
-    prefs: {
-        'network.proxy.type': 0
-    }
-}
-
-},
+    browsers: ['Chrome'],
     singleRun: false,
+    // customLaunchers: {
+    //   ChromeHeadlessNoSandbox: {
+    //     base: 'ChromeHeadless',
+    //     flags: ['--no-sandbox']
+    //   }
+    // },
     restartOnFileChange: true
   });
 };
